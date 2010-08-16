410c7335ef527f720876e1864a59c79f92858907
Aaron Peckham <apeckham@gmail.com>
2010-08-15 22:04:51 -0700

    show quoted words and defids

--- public/script.js
+++ public/script.js
@@ -1,18 +1,26 @@
 var success = function(transport, element) {
   element = element || $("my-widget");
   var description = transport.responseJSON.description;
-  var match = description.match(/defid=(\d+)/);
-  
-  if (match) {
-    element.update(match[1]);
-  } else {
-    element.update("no defids");
+  description = description.split("------------------")[0];
+
+  var matches = [];
+
+  var pattern = /defid=(\d+)/g;
+  while (match = pattern.exec(description)) {
+    matches.push(match[1]);
   }
+
+  var pattern = /"([^"]+)"/g;
+  while (match = pattern.exec(description)) {
+    matches.push(match[1]);
+  }
+  
+  element.update(matches.join(" "));
 };
 
-// if (window.location.href.match(/ticket/)) {
+if (window.location.href.match(/ticket/)) {
   new Ajax.Request(window.location.href + ".json", {
     method: "GET",
     onSuccess: success
   });
-// }
+}
