2e856083dbd6f09665fcb34841dfff8042997717
Aaron Peckham <apeckham@gmail.com>
2010-08-16 09:56:27 -0700

    extract model

--- public/script.js
+++ public/script.js
@@ -1,21 +1,34 @@
+var Ticket = Class.create({
+  initialize: function(json) {
+    this.json = json;
+  },
+  
+  getMatches: function() {
+    var description = this.json.description;
+    description = description.split("------------------")[0];
+
+    var matches = [];
+
+    function addMatches(pattern) {
+      while (match = pattern.exec(description)) {
+        matches.push(match[1]);
+      }
+    }
+
+    addMatches(/defid=(\d+)/g);
+    addMatches(/"([^"]{0,30})"/g);
+    addMatches(/'([^']{0,30})'/g);
+    
+    return matches;
+  }
+});
+
 var success = function(transport, element) {
   element = element || $("my-widget");
-  var description = transport.responseJSON.description;
-  description = description.split("------------------")[0];
-
-  var matches = [];
   
-  function addMatches(pattern) {
-    while (match = pattern.exec(description)) {
-      matches.push(match[1]);
-    }
-  }
-
-  addMatches(/defid=(\d+)/g);
-  addMatches(/"([^"]{0,30})"/g);
-  addMatches(/'([^']{0,30})'/g);
+  var ticket = new Ticket(transport.responseJSON);
   
-  element.update(matches.join(" "));
+  element.update(ticket.getMatches().join(" "));
 };
 
 if (window.location.href.match(/ticket/)) {
