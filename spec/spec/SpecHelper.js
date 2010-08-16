f61d5ae2ef70d0575425aa3c005f5bdc1b1fef4e
Aaron Peckham <apeckham@gmail.com>
2010-08-16 10:27:05 -0700

    use getInnerText method

--- spec/spec/SpecHelper.js
+++ spec/spec/SpecHelper.js
@@ -1,2 +1,7 @@
-afterEach(function() {
+Element.addMethods({  
+  getInnerText: function(element) {
+    element = $(element);
+    return element.innerText && !window.opera ? element.innerText
+      : element.innerHTML.stripScripts().unescapeHTML().replace(/[\n\r\s]+/g, ' ');
+  }
 });
