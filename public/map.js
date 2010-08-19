72cf0aea3bbaccb52c88dcd419ad0e9385d9229f
Aaron Peckham <apeckham@gmail.com>
2010-08-19 13:46:31 -0700

    refactor

--- public/map.js
+++ public/map.js
@@ -1,15 +1,11 @@
-var geoPlugin = function(geoInfo) {
-  var latlng = new google.maps.LatLng(geoInfo.geoplugin_latitude, geoInfo.geoplugin_longitude);
-  var myOptions = {
-    zoom: 4,
-    center: latlng,
-    mapTypeId: google.maps.MapTypeId.ROADMAP
-  };
-  var map = new google.maps.Map(document.getElementById("map_canvas"), myOptions);
+var geoPlugin = function(geoplugin) {
+  var latlng = new google.maps.LatLng(geoplugin.geoplugin_latitude, geoplugin.geoplugin_longitude);
+  var options = {zoom: 4, center: latlng, mapTypeId: google.maps.MapTypeId.ROADMAP};
+  new google.maps.Map($("map_canvas"), options);
 }
 
-var mapLoaded = function() {
-  Widget.require('http://www.geoplugin.net/json.gp?ip=86.34.204.8', {type: 'text/javascript'})
+var mapsCallback = function() {
+  Widget.require('http://www.geoplugin.net/json.gp?ip=86.34.204.8', {type: 'text/javascript'});
 }
 
-Widget.require('http://maps.google.com/maps/api/js?sensor=false&callback=mapLoaded', {type: 'text/javascript'});
+Widget.require('http://maps.google.com/maps/api/js?sensor=false&callback=mapsCallback', {type: 'text/javascript'});
