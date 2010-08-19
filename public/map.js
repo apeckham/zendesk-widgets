061f34d6e897056c60aed76c9bae1c84c6efc1f4
Aaron Peckham <apeckham@gmail.com>
2010-08-19 13:41:58 -0700

    show map of where the user is located

--- public/map.js
+++ public/map.js
@@ -1,13 +1,15 @@
+var geoPlugin = function(geoInfo) {
+  var latlng = new google.maps.LatLng(geoInfo.geoplugin_latitude, geoInfo.geoplugin_longitude);
+  var myOptions = {
+    zoom: 8,
+    center: latlng,
+    mapTypeId: google.maps.MapTypeId.ROADMAP
+  };
+  var map = new google.maps.Map(document.getElementById("map_canvas"), myOptions);
+}
+
 var mapLoaded = function() {
-  // Widget.require('http://www.geoplugin.net/json.gp?ip=xx.xx.xx.xx', {type: 'text/javascript'})
-  
-    var latlng = new google.maps.LatLng(-34.397, 150.644);
-    var myOptions = {
-      zoom: 8,
-      center: latlng,
-      mapTypeId: google.maps.MapTypeId.ROADMAP
-    };
-    var map = new google.maps.Map(document.getElementById("map_canvas"), myOptions);
+  Widget.require('http://www.geoplugin.net/json.gp?ip=xx.xx.xx.xx', {type: 'text/javascript'})
 }
 
 Widget.require('http://maps.google.com/maps/api/js?sensor=false&callback=mapLoaded', {type: 'text/javascript'});
