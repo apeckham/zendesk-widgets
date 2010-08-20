916ee0c6a53ef3b1054b88a3765dff3923cc61a4
Aaron Peckham <apeckham@gmail.com>
2010-08-20 01:20:24 -0700

    refactor

--- public/map.js
+++ public/map.js
@@ -2,15 +2,18 @@ window.Urban = window.Urban || {};
 
 Urban.Map = Class.create({
   initialize: function(element) {
-    window.geoPlugin = function(geoplugin) {
-      var latlng = new google.maps.LatLng(geoplugin.geoplugin_latitude, geoplugin.geoplugin_longitude);
-      var options = {zoom: 6, center: latlng, mapTypeId: google.maps.MapTypeId.ROADMAP};
-      new google.maps.Map(element, options);
-    };
-
+    this.element = element;
+    
+    window.geoPlugin = this.geoPlugin.bind(this);
     window.mapsLoaded = this.mapsLoaded.bind(this);
     
     loadScript('http://maps.google.com/maps/api/js?sensor=false&callback=mapsLoaded');
+  },
+  
+  geoPlugin: function(geoplugin) {
+    var latlng = new google.maps.LatLng(geoplugin.geoplugin_latitude, geoplugin.geoplugin_longitude);
+    var options = {zoom: 6, center: latlng, mapTypeId: google.maps.MapTypeId.ROADMAP};
+    new google.maps.Map(this.element, options);
   },
   
   mapsLoaded: function() {
