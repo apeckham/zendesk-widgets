8c2a227e10f9975bda948d8aa98bd009c021cb68
Aaron Peckham <apeckham@gmail.com>
2010-08-20 01:27:09 -0700

    refactor

--- public/map.js
+++ public/map.js
@@ -4,25 +4,27 @@ Urban.Map = Class.create({
   initialize: function(element) {
     this.element = element;
     
-    window.geoPlugin = this.geoPlugin.bind(this);
+    window.geoPlugin = this.geodataLoaded.bind(this);
     window.mapsLoaded = this.mapsLoaded.bind(this);
     loadScript('http://maps.google.com/maps/api/js?sensor=false&callback=mapsLoaded');
   },
   
-  geoPlugin: function(geoplugin) {
-    new Urban.Map.View(this.element, geoplugin);
+  mapsLoaded: function() {
+    Urban.Ticket.load(this.ticketLoaded.bind(this));
+  },
+
+  ticketLoaded: function(ticket) {
+    loadScript('http://www.geoplugin.net/json.gp?ip=' + ticket.getServerParameters().getIp());
   },
   
-  mapsLoaded: function() {
-    Urban.Ticket.load(function(ticket) {
-      loadScript('http://www.geoplugin.net/json.gp?ip=' + ticket.getServerParameters().getIp());
-    });
+  geodataLoaded: function(geodata) {
+    new Urban.Map.View(this.element, geodata);
   }
 });
 
 Urban.Map.View = Class.create({
-  initialize: function(element, geoplugin) {
-    var latLng = new google.maps.LatLng(geoplugin.geoplugin_latitude, geoplugin.geoplugin_longitude);
+  initialize: function(element, geodata) {
+    var latLng = new google.maps.LatLng(geodata.geoplugin_latitude, geodata.geoplugin_longitude);
     new google.maps.Map(element, {zoom: 6, center: latLng, mapTypeId: google.maps.MapTypeId.ROADMAP});
   }
 });
