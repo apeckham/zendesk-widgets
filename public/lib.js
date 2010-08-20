1290001f4b26bce8c3731bb1b657e2a57221a254
Aaron Peckham <apeckham@gmail.com>
2010-08-19 18:03:26 -0700

    show map of where the user's located

--- public/lib.js
+++ public/lib.js
@@ -89,15 +89,17 @@ Urban.Map = Class.create({
   initialize: function(element) {
     window.geoPlugin = function(geoplugin) {
       var latlng = new google.maps.LatLng(geoplugin.geoplugin_latitude, geoplugin.geoplugin_longitude);
-      var options = {zoom: 4, center: latlng, mapTypeId: google.maps.MapTypeId.ROADMAP};
+      var options = {zoom: 6, center: latlng, mapTypeId: google.maps.MapTypeId.ROADMAP};
       new google.maps.Map(element, options);
     };
 
-    window.mapsCallback = function() {
-      loadScript('http://www.geoplugin.net/json.gp?ip=86.34.204.8');
+    window.mapsLoaded = function() {
+      Urban.Ticket.load(function(ticket) {
+        loadScript('http://www.geoplugin.net/json.gp?ip=' + ticket.getServerParameters().getIp());
+      });
     };
     
-    loadScript('http://maps.google.com/maps/api/js?sensor=false&callback=mapsCallback');
+    loadScript('http://maps.google.com/maps/api/js?sensor=false&callback=mapsLoaded');
   }
 });
 
